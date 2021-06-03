662c3e625e3cfbb9c87c2c2566ea7a38f8f34642
Dawid Cieszyński <dcieszynski99@gmail.com>
2021-06-03 03:58:43 +0200

    update_and_orderby_marketcap

--- index.js
+++ index.js
@@ -89,15 +89,18 @@ const createCoinListElement = (coins) => {
     coinRow.classList.add("coin-row");
     const logoImg = document.createElement("img");
     logoImg.src = coin.image;
+    logoImg.classList.add("logo-td");
     const nameTd = document.createElement("td");
     nameTd.innerText = coin.name;
     nameTd.classList.add("coin-td");
+    nameTd.classList.add("name-td");
     const priceTd = document.createElement("td");
     priceTd.innerText = `${coin.current_price}USD`;
-    priceTd.classList.add("price-td");
     priceTd.classList.add("coin-td");
+    priceTd.classList.add("price-td");
     const changeTd = document.createElement("td");
     changeTd.classList.add("coin-td");
+    changeTd.classList.add("change-td");
     const priceChange = Math.round(
       (coin.price_change_percentage_24h * 100) / 100
     );
@@ -132,6 +135,30 @@ const convertCoinsRowsIds = (coinRowsIds) => {
   return coinRowsIdsString;
 };
 
+const getCoinsIds = (coinsData) => {
+  const coinsIds = [];
+  coinsData.forEach((coinData) => {
+    coinsIds.push(coinData.id);
+  });
+  return coinsIds;
+};
+
+const getCoinsLogos = (coinsData) => {
+  const coinsLogos = [];
+  coinsData.forEach((coinData) => {
+    coinsLogos.push(coinData.image);
+  });
+  return coinsLogos;
+};
+
+const getCoinsNames = (coinsData) => {
+  const coinsNames = [];
+  coinsData.forEach((coinData) => {
+    coinsNames.push(coinData.name);
+  });
+  return coinsNames;
+};
+
 const getCoinsPrices = (coinsData) => {
   const coinsPrices = [];
   coinsData.forEach((coinData) => {
@@ -140,7 +167,7 @@ const getCoinsPrices = (coinsData) => {
   return coinsPrices;
 };
 
-const getCoinChanges = (coinsData) => {
+const getCoinsChanges = (coinsData) => {
   const coinsChanges = [];
   coinsData.forEach((coinData) => {
     coinsChanges.push(
@@ -157,19 +184,50 @@ const getCoinsData = async (coinRowsIdsString, coinRowsIds) => {
     )
     .then((res) => {
       const coinsData = res.data;
+      const coinsIds = getCoinsIds(coinsData);
+      const coinsLogos = getCoinsLogos(coinsData);
+      const coinsNames = getCoinsNames(coinsData);
       const coinsPrices = getCoinsPrices(coinsData);
-      const coinsChanges = getCoinChanges(coinsData);
+      const coinsChanges = getCoinsChanges(coinsData);
+
+      const coinsTrIds = document.querySelectorAll(".coin-row");
+      const coinTrIdsArray = Array.from(coinsTrIds);
+      coinsTrIds.forEach((coinTrId) => {
+        const idIndex = coinTrIdsArray.indexOf(coinTrId);
+        coinTrId.id = `${coinsIds[idIndex]}-el`;
+      });
+
+      const coinsTdLogo = document.querySelectorAll(".logo-td");
+      const coinTdLogoArray = Array.from(coinsTdLogo);
+      coinsTdLogo.forEach((coinTdLogo) => {
+        const logoIndex = coinTdLogoArray.indexOf(coinTdLogo);
+        coinTdLogo.src = coinsLogos[logoIndex];
+      });
+
+      const coinsTdName = document.querySelectorAll(".name-td");
+      const coinTdNameArray = Array.from(coinsTdName);
+      coinsTdName.forEach((coinTdName) => {
+        const nameIndex = coinTdNameArray.indexOf(coinTdName);
+        coinTdName.innerText = `${coinsNames[nameIndex]}`;
+      });
+
       const coinsTdPrice = document.querySelectorAll(".price-td");
       const coinTdPriceArray = Array.from(coinsTdPrice);
       coinsTdPrice.forEach((coinTdPrice) => {
-        const coinIndex = coinTdPriceArray.indexOf(coinTdPrice);
-        coinTdPrice.innerText = `${coinsPrices[coinIndex]}USD`;
+        const priceIndex = coinTdPriceArray.indexOf(coinTdPrice);
+        coinTdPrice.innerText = `${coinsPrices[priceIndex]}USD`;
       });
-      /*const coinsTdChange = document.querySelectorAll(".change-td");
+
+      const coinsTdChange = document.querySelectorAll(".change-td");
+      const coinsTdChangeArray = Array.from(coinsTdChange);
       coinsTdChange.forEach((coinTdChange) => {
-        const coinIndex = coinsTdChange.indexOf(coinTdChange);
-        coinTdChange.innerText = `${coinsChanges[coinIndex]}%`;
-      });*/
+        const changeIndex = coinsTdChangeArray.indexOf(coinTdChange);
+        coinTdChange.innerText = `${coinsChanges[changeIndex]}%`;
+      });
+
+      console.log(coinsData);
+      console.log(coinsLogos);
+      console.log(coinsNames);
       console.log(coinsPrices);
       console.log(coinsChanges);
       console.log(coinRowsIds);
